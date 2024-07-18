5ee602778b38cf23a53e51d07a4611e526169a1c
glebis <glebis@gmail.com>
2024-07-18 02:13:09 +0200

    Ignored .aider* files in .gitignore and updated jest.config.js to use a single tsconfig.test.json file.

--- jest.config.js
+++ jest.config.js
@@ -2,19 +2,11 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.tsx?$': ['ts-jest', {
-      tsconfig: 'tsconfig.test.json'
-    }]
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.test.json' }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
   moduleNameMapper: {
-    '^obsidian$': '<rootDir>/__mocks__/obsidian.ts'
+    '^obsidian$': '<rootDir>/__mocks__/obsidian.ts',
   },
-
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.test.json'
-    }
-  }
 };
